a8644e51023e0d0796b88d2062e44cefdfcaf5bc
Gitreposuser <vlad77solo@gmail.com>
2022-09-13 11:38:03 +0200

    working on card functionality

--- src/app/pages/game1/game1.component.ts
+++ src/app/pages/game1/game1.component.ts
@@ -5,11 +5,57 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './game1.component.html',
   styleUrls: ['./game1.component.css']
 })
+
 export class Game1Component implements OnInit {
-  items = new Array<number>(9);
+  length = 9;
+  items = new Array<number>(length);
+  cards = new Array<number>(this.length);
 
   constructor() { 
+    for(let i = 0; i < this.length; i++)
+    {
+      this.items[i] = 0;
+    }
   }
+  
   ngOnInit(): void {
+    this.initCardStack()
+  }
+
+  mixCardDeck(): void {
+    let buffer = 0;
+    for(let i = 0; i < 100; i++)
+    {
+      let ind1 = Math.floor(Math.random() * this.length);
+      let ind2 = Math.floor(Math.random() * this.length);
+
+      buffer = this.cards[ind1];
+      this.cards[ind1] = this.cards[ind2];
+      this.cards[ind2] = buffer;
+    }
+  }
+
+  generateCard(): number {
+    let num = Math.floor(Math.random() * 10);
+
+    if(5 < num) 
+    {
+      return 1;
+    }
+    else 
+    {
+      return 0;
+    }
+  }
+
+  initCardStack(): void{
+    for(let i = 0; i < this.cards.length; i++){
+      this.cards[i] = this.generateCard();
+    }
+    this.mixCardDeck();
+  }
+
+  onClick(): void {
+     
   }
 }
