928ac7ee0146d81fd573bc99e8bd628206db6f46
Gitreposuser <vlad77solo@gmail.com>
2022-09-17 00:33:00 +0200

    add about page, base functionality

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -6,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  path: string;
+  photo: string;
 
-  constructor() { }
+  constructor(private router: Router) { 
+    this.path = "../../assets/images/";
+    this.photo = "catphoto.png";
+  }
 
   ngOnInit(): void {
+  }
+
+  onBackClick(): void{
+    this.router.navigate(['home']);
+  }
+
+  onImgClick(): void{
+    if("catphoto.png" === this.photo)
+    {
+      this.photo = "logoclean.png";
+    }
+    else
+    {
+      this.photo = "catphoto.png";
+    }
   }
 }
